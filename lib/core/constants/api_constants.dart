dc9ff8000a050351a40f87cf8eb8cf00518ba113
mhmmdfz12 <2306041@itg.ac.id>
2026-07-09 21:02:28 +0700

    Update baseUrl

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -5,10 +5,11 @@ class ApiConstants {
 
   // Android emulator tidak dapat mengakses localhost host machine secara langsung.
   static String get baseUrl {
-    if (kIsWeb) return 'http://localhost:3000/api';
+    if (kIsWeb)
+      return 'https://backendprakmobile-production.up.railway.app/api';
     if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:3000/api';
+      return 'https://backendprakmobile-production.up.railway.app/api';
     }
-    return 'http://localhost:3000/api';
+    return 'https://backendprakmobile-production.up.railway.app/api';
   }
 }
